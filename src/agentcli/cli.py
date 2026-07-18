89e6f27a9d180a9c87905d64665a69d4b80b0fb3
robert baxter <baxterrp@gmail.com>
2026-07-18 10:50:32 -0400

    feat: working call to azure ai

--- src/agentcli/cli.py
+++ src/agentcli/cli.py
@@ -1,2 +1,15 @@
+from dotenv import load_dotenv
+import typer
+
+from agentcli.providers.azure_llm_provider import AzureLLMProvider
+
+
+def main():
+    provider = AzureLLMProvider()
+    for chunk in provider.ask("Write a poem about a cat."):
+        print(chunk, end="", flush=True)
+
+
 def cli():
-    print("Welcome to the Agent CLI!")
+    load_dotenv()
+    typer.run(main)
